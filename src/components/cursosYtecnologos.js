83a64e860573fd09e342f12c32872a0b4ff1f3b7
iTTZBASTIAN <navarrolopezsebastian125@gmail.com>
2025-12-05 21:07:11 -0500

    componente Educacion dinamico con formación complementaria

---
+++ src/components/cursosYtecnologos.js
@@ -0,0 +1,22 @@
+const cursosYTecnologos = [
+  {
+    tipo: "Tecnólogo",
+    nombre: "Tecnología en Informática Musical",
+    institucion: "ITM",
+    duracion: "2-3 años",
+  },
+  {
+    tipo: "Técnico",
+    nombre: "Ejecución Musical con Instrumentos Funcionales",
+    institucion: "SENA",
+    duracion: "1-2 años",
+  },
+  {
+    tipo: "Tecnólogo",
+    nombre: "Tecnología en Construcción de Acabados Arquitectónicos",
+    institucion: "ITM",
+    duracion: "2-3 años",
+  },
+];
+
+export default cursosYTecnologos;
